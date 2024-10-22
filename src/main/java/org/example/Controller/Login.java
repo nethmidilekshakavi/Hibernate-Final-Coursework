f14042ff5e1ee9ac793a212ea8b6c2adf50645bc
ASUS <nethmidileksha6@gmail.com>
2024-10-22 11:59:56 +0530

    create the layered Structure

---
+++ src/main/java/org/example/Controller/Login.java
@@ -0,0 +1,56 @@
+package org.example.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class Login {
+
+    @FXML
+    private Text ForgotPW;
+
+    @FXML
+    private Text OR;
+
+    @FXML
+    private Button SignInBtn;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private ImageView loginPic;
+
+    @FXML
+    private AnchorPane loginpage;
+
+    @FXML
+    private ImageView passwordpic;
+
+    @FXML
+    private TextField passwordtxt;
+
+    @FXML
+    private Text topic;
+
+    @FXML
+    private ImageView usernamepic;
+
+    @FXML
+    private TextField usernametxt;
+
+    @FXML
+    void SigninOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginOnAction(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/org/example/Controller/RegisterTheSystem.java
@@ -0,0 +1,35 @@
+package org.example.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class RegisterTheSystem {
+
+    @FXML
+    private AnchorPane ResgisterTheSystemPage;
+
+    @FXML
+    private Text SignUp;
+
+    @FXML
+    private TextField passwordtxt;
+
+    @FXML
+    private Button signUpbtn;
+
+    @FXML
+    private Text topic;
+
+    @FXML
+    private TextField usernametxt;
+
+    @FXML
+    void SignUpOnAction(ActionEvent event) {
+
+    }
+
+}
